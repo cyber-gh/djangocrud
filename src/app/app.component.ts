9e74ff471273f4df264a2aa751cc52006ccc2f67
cybergh <solgheorghe@gmail.com>
2019-04-17 22:16:48 +0300

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import {ApiService} from "./api.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ApiService]
 })
 export class AppComponent {
   title = 'frontend';
+  movies = [{title: 'test'}];
+
+  constructor(private api:ApiService) {
+    this.getMovies();
+  }
+
+  getMovies = () => {
+    this.api.getAllMovies().subscribe(
+      data => {
+        this.movies = data.results;
+        console.log(data.results);
+      },
+      error => {
+        console.log(error);
+      }
+    )
+  }
 }
